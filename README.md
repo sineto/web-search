6bffdbe05a7d1804f59e25d52823b96e99d57d23
Sinésio Neto <sinetoami@gmail.com>
2018-11-29 03:37:43 -0300

    update plugin: - add readme - add github search engine - add youtube search engine - fix yahoo search syntax

---
+++ README.md
@@ -0,0 +1,33 @@
+web-search plugin
+-----------------
+This plugin allow aliases for searching from terminal with some popular web search engines.
+
+## Usage
+`web-search` plugin give to you alias for `web_search` command. You can use this for example:
+```
+# using web_search command
+$ web_search google zsh
+
+# using alias
+$ google zsh
+```
+The results are the same for both ways.
+
+## Search engines supported
+| alias     | engine                                      |
+| ---       | ---                                         |
+| `ddg`     | `https://www.duckduckgo.com/?q=`            |
+| `google`  | `https://www.google.com/search?q=`          |
+| `yahoo`   | `https://search.yahoo.com/search?p=`        |
+| `github`  | `https://github.com/search?q=`              |
+| `youtube` | `https://youtube.com/results?search_query=` |
+| `bing`    | `https://www.bing.com/search?q=`            |
+
+## Plus+ DuckDuckGo
+| alias    | description                        |
+| ---      | ---                                |
+| `wiki`   | open result with Wikipedia         |
+| `news`   | open result with DuckDuckGo News   |
+| `map`    | open result with Google Maps       |
+| `images` | open result with DuckDuckGo Images |
+| `ducky`  | open the first result              |
--- web_search.plugin.zsh
+++ web_search.plugin.zsh
@@ -6,7 +6,7 @@ function web_search() {
   local open_cmd
   [[ "$OSTYPE" = linux* ]] && open_cmd='xdg-open'
 
-  pattern='(google|duckduckgo|bing|yahoo)'
+  pattern='(google|duckduckgo|bing|yahoo|github|youtube)'
 
   # check whether the search engine is supported
   if [[ $1 =~ pattern ]];
@@ -15,19 +15,25 @@ function web_search() {
     return 1
   fi
 
-  local url="http://www.$1.com"
+  local url
+  [[ "$1" == 'yahoo' ]] && url="https://search.yahoo.com" || url="https://www.$1.com"
 
   # no keyword provided, simply open the search engine homepage
   if [[ $# -le 1 ]]; then
     $open_cmd "$url"
     return
   fi
-  if [[ $1 == 'duckduckgo' ]]; then
-  # slightly different search syntax for DDG
-    url="${url}/?q="
-  else
-    url="${url}/search?q="
-  fi
+
+  typeset -A search_syntax=(
+    google     "/search?q="
+    bing       "/search?q="
+    github     "/search?q="
+    duckduckgo "/?q="
+    yahoo      "/search?p="
+    youtube    "/results?search_query="
+  )
+
+  url="${url}${search_syntax[$1]}"
   shift   # shift out $1
 
   while [[ $# -gt 0 ]]; do
@@ -43,11 +49,12 @@ alias bing='web_search bing'
 alias google='web_search google'
 alias yahoo='web_search yahoo'
 alias ddg='web_search duckduckgo'
+alias github='web_search github'
+alias youtube='web_search youtube'
 
 #add your own !bang searches here
 alias wiki='web_search duckduckgo \!w'
 alias news='web_search duckduckgo \!n'
-alias youtube='web_search duckduckgo \!yt'
 alias map='web_search duckduckgo \!m'
 alias image='web_search duckduckgo \!i'
 alias ducky='web_search duckduckgo \!'
